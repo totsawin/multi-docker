ada62472f38cffb0bcd3aa0c5430e31cd5a96caa
windisnothere <i_amnothere@live.com>
2019-04-16 17:09:49 +0700

    unit test client app in CI

--- client/src/karma.conf.js
+++ client/src/karma.conf.js
@@ -25,7 +25,17 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadlessNoSandbox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox',
+          '--user-data-dir=/tmp/chrome-test-profile',
+          'disable-web-security'
+        ]
+      }
+    },
     singleRun: false
   });
 };
